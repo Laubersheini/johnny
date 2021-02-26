478c3d0932e14980138536781538fedf4a691368
David Laubersheimer <d.laubersheimer@t-online.de>
2021-02-26 20:46:10 +0100

    Make the service worker cache everything, offline first

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+//var cacheName = "johnny-cache-v.2.1.0" //change this if anything has changed on the site
+var cacheName = "johnny-cache-v.2.1.0" //change this if anything has changed on the site
+
+
+
+//this implements a chache first approach for everything the Website fetches
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+        //console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+
+
+
+
+self.addEventListener('activate', event => {
+//  console.log("fgdhjfgh");
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+
+        if (key.includes("johnny-cache")) {
+          return caches.delete(key);
+        }
+      })
+
+    )).then(() => {
+        console.log('V2.1.0 now ready to handle fetches!');
+        clients.claim()
+    })
+  );
+});
